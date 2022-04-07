0d6ec6f8daf45bef946b8796d3cab3848b982808
ayaayharvey <ayaayharvey@gmail.com>
2022-04-07 18:03:07 +0800

    new

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+var first = 0;
+var last = 10;
+var data = [];
+var max = false;
+
 fetch("https://api.spacexdata.com/v4/launches/")
   .then((response) => {
     if (response.ok) {
@@ -7,80 +12,50 @@ fetch("https://api.spacexdata.com/v4/launches/")
     }
   })
   .then((data) => {
-    console.log(data);
     displayRecord(data);
+    this.data = data;
   })
   .catch((error) => console.error("FETCH ERROR:", error));
 
 function displayRecord(data) {
-  //   const cocktailIngredients = document.createElement("ul");
-
+  document.getElementById("loading").style.display = "none";
   const list = document.getElementById("list-container");
   const listItems = document.createElement("ul");
   list.appendChild(listItems);
   data.map((record, i) => {
-    // console.log(i.success);
-    if (i < 10) {
+    if (i >= this.first && i <= this.last) {
       listItem = document.createElement("li");
       listItemArticle = document.createElement("article");
       listItemArticleImage = document.createElement("img");
       listItemArticleDetails = document.createElement("div");
       listItemArticleDetails.classList.add("details");
+      listItemArticleDetailsTitle = document.createElement("h2");
+      listItemArticleDetailsDescription = document.createElement("p");
+
       listItemArticleImage.src = record.links.patch.small;
-      //   listItem.innerHTML = record.links.patch.small;
       listItemArticle.appendChild(listItemArticleImage);
+
+      listItemArticleDetailsTitle.innerHTML = record.name;
+      listItemArticleDetails.appendChild(listItemArticleDetailsTitle);
+
+      listItemArticleDetailsDescription.innerHTML = record.details
+        ? record.details
+        : "No Description";
+      listItemArticleDetails.appendChild(listItemArticleDetailsDescription);
+
       listItemArticle.appendChild(listItemArticleDetails);
+
       listItem.appendChild(listItemArticle);
       listItems.appendChild(listItem);
+      list.appendChild(listItems);
     }
   });
+}
 
-  //   for (let datas in data) {
-  //     console.log(datas);
-  // const list = document.getElementById("list-container");
-
-  // const listItems = document.createElement("ul");
-  // list.appendChild(listItems);
-
-  // for (let key in getIngredients) {
-  //         let value = getIngredients[key];
-  //         listItem = document.createElement("li");
-  //         listItem.innerHTML = value;
-  //         cocktailIngredients.appendChild(listItem);
-  //       }
-
-  // let value = datas.success;
-  // listItem = document.createElement("li");
-  // listItem.innerHTML = value;
-  //   }
-
-  //   const cocktail = data.drinks[0];
-  //   const cocktailDiv = document.getElementById("cocktail");
-  //   const cocktailName = cocktail.strDrink;
-  //   const heading = document.createElement("h1");
-  //   heading.innerHTML = cocktailName;
-  //   cocktailDiv.appendChild(heading);
-  //   const cocktailImg = document.createElement("img");
-  //   cocktailImg.src = cocktail.strDrinkThumb;
-  //   cocktailDiv.appendChild(cocktailImg);
-  //   const cocktailIngredients = document.createElement("ul");
-  //   cocktailDiv.appendChild(cocktailIngredients);
-
-  //   const getIngredients = Object.keys(cocktail)
-  //     .filter(function (ingredient) {
-  //       return ingredient.indexOf("strIngredient") == 0;
-  //     })
-  //     .reduce(function (ingredients, ingredient) {
-  //       if (cocktail[ingredient] != null) {
-  //         ingredients[ingredient] = cocktail[ingredient];
-  //       }
-  //       return ingredients;
-  //     }, {});
-
-  //   for (let key in getIngredients) {
-  //     let value = getIngredients[key];
-  //     listItem = document.createElement("li");
-  //     listItem.innerHTML = value;
-  //     cocktailIngredients.appendChild(listItem);
-  //   }
+function moreRecords() {
+  this.first += 10;
+  this.last += 10;
+  displayRecord(this.data);
+  if (this.last > this.data.length)
+    document.getElementById("seeMore").style.display = "none";
 }
